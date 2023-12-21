c252fe9c837a44ad302e4bb60d309556fd37c181
DarkLordOfDeepSpace <154472708+DarkLordOfDeepSpace@users.noreply.github.com>
2023-12-21 23:43:02 +0300

    Delete subtask2.py

--- subtask2.py
+++
@@ -1,7 +0,0 @@
-n=int(input("Please, type a number: \n"))
-q=0
-while (q**2)<=n:
-    q=q+1
-    print(q)
-print("The largest square number q less than or equal to input equals ",(q-1)**2)
-# it looks like I learned how to use git today
