5636d5fd84ae8fc2bfbb4a286441c73c074243d0
DarkLordOfDeepSpace <154472708+DarkLordOfDeepSpace@users.noreply.github.com>
2023-12-21 23:47:31 +0100

    Add files via upload

---
+++ subtask2.py
@@ -0,0 +1,16 @@
+x=[int(n) for n in input("Type in elements of a list separated by space \n").split()]
+n=len(x)
+s=0
+if n==0:
+    m=-1
+    a=-1
+else:
+    n=0
+    m=x[0]
+    while x[n]!=-1:
+        s=s+x[n]
+        if m>x[n] and x[n]!=-1:
+            m=x[n]
+        n=n+1
+    a=s/n
+print("Count: ",n,"\nSum: ",s,"\nMinimum: ",m,"\nMean: ",a)
